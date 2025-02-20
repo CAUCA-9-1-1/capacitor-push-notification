b279f6a1b9025d8c1411568f49bfbc2d9b2df49d
Frédéric Catellier-Poulin <frederic.catellier@cauca.ca>
2025-02-20 14:53:38 -0500

    Version CAUCA du plugin

--- ios/Sources/PushNotificationsPlugin/PushNotificationsHandler.swift
+++ ios/Sources/PushNotificationsPlugin/PushNotificationsHandler.swift
@@ -6,7 +6,22 @@ public class PushNotificationsHandler: NSObject, NotificationHandlerProtocol {
     var notificationRequestLookup = [String: JSObject]()
 
     public func requestPermissions(with completion: ((Bool, Error?) -> Void)? = nil) {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+        var requestAuthorizationOptions: UNAuthorizationOptions = []
+        if #available(iOS 12.0, *) {
+            requestAuthorizationOptions = [.alert, .sound, .badge, .criticalAlert]
+        } else {
+            requestAuthorizationOptions = [.alert, .sound, .badge]
+        }
+
+        UNUserNotificationCenter.current().requestAuthorization(options: requestAuthorizationOptions) { granted, error in
+            if let error = error {
+                NSLog("An error occured \(error.localizedDescription)")
+            }
+
+            UNUserNotificationCenter.current().getNotificationSettings { settings in
+                NSLog("Current notification settings \(settings.debugDescription)")
+            }
+
             completion?(granted, error)
         }
     }
@@ -38,11 +53,10 @@ public class PushNotificationsHandler: NSObject, NotificationHandlerProtocol {
                     presentationOptions.insert(.alert)
                 case "badge":
                     presentationOptions.insert(.badge)
-
                 case "sound":
                     presentationOptions.insert(.sound)
                 default:
-                    print("Unrecogizned presentation option: \(option)")
+                    print("Unrecognized presentation option: \(option)")
                 }
             }
 
